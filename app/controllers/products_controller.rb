c5ff78d7bdd319606993e9e39e1491d887f483f3
johnnythecakes <1zachjohnson@gmail.com>
2014-08-03 15:23:05 -0700

    added products controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,42 @@
 class ProductsController < ApplicationController
+
+	def index
+		@products = Product.all? 
+	end
+
+	def show
+		@product = Product.find(params:[:id])
+	end
+
+	def new
+		@product = Product.new
+	end
+
+	def create
+		@product = Product.new(params.require(:product).permit(:product_name, :product_purchase_price, :product_estimate_price, :product_category_id, :product_make, :product_warranty, :product_warranty_length))
+		if @product.save
+			redirect_to products_path
+		else
+			render'new'
+		end
+	end
+
+	def edit
+		@product = Product.find(params[:id])
+	end
+
+	def update
+		@product = Product.find(params[:id])
+		if @product.update_attributes(params.require(:product).permit(:product_name, :product_purchase_price, :product_estimate_price, :product_category_id, :product_make, :product_warranty, :product_warranty_length))
+			redirect_to products_path
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@product = Product.find(params[:id])
+		@products.destroy
+		redirect_to products_path
+	end
 end
